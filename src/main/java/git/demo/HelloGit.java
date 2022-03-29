536637059b81742361365db2a41ed5d3c68606a2
Haiming <Haiming@126.com>
2022-03-29 17:45:54 +0800

    2

--- src/main/java/git/demo/HelloGit.java
+++ src/main/java/git/demo/HelloGit.java
@@ -3,5 +3,8 @@ package git.demo;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git1!!!");
+        System.out.println("hello git2!!!");
+        System.out.println("hello git3!!!");
     }
 }
